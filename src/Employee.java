a78b484041ecc51efae241f65e8cb4811c20b478
melekhin <melekhinmikhail14@gmail.com>
2022-11-03 23:07:30 +0300

    CourseWork done

---
+++ src/Employee.java
@@ -0,0 +1,44 @@
+public class Employee {
+    private String fullName;
+    private int department;
+    private double salary;
+    private int id;
+
+    public Employee(String fullName, int department, double salary, int id) {
+        this.fullName = fullName;
+        if (department < 1 || department > 5) throw new IllegalArgumentException("Отдел может быть от 1 до 5");
+        else this.department = department;
+        if (salary < 0) throw new IllegalArgumentException("Зарплата не может быть отрицательной");
+        else this.salary = salary;
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public double setSalary(double salary) {
+        return this.salary = salary;
+    }
+
+    public int setDepartment(int department) {
+        return this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО: "+fullName+"  Отдел: "+department+"  Зарплата: "+salary+" руб.  ID: "+id;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int id = 0;
+        EmployeeBook company = new EmployeeBook(10);
+        company.addNewEmployee("Джонсон Олег Владимирович", 2, 100_000, id++);
+        company.addNewEmployee("Соловьев Игорь Сергеевич", 3, 120_000, id++);
+        company.addNewEmployee("Петров Валентин Михайлович", 1, 150_000, id++);
+        company.addNewEmployee("Иванов Сергей Геннадьевич", 5, 90_000, id++);
+        company.addNewEmployee("Соколова Наталья Семёновна", 4, 130_000, id++);
+        company.addNewEmployee("Киркоров Матвей Савельевич", 2, 200_000, id++);
+        company.addNewEmployee("Апаткин Владислав Андреевич", 4, 115_000, id++);
+        company.addNewEmployee("Меркель Зиноида Ивановна", 3, 137_000, id++);
+        company.addNewEmployee("Пушкин Анатолий Дмитриевич", 1, 180_000, id++);
+        company.addNewEmployee("Фёдоров Андрей Валерьевич", 5, 250_000, id++);
+        company.printAllEmployees();
     }
 }
